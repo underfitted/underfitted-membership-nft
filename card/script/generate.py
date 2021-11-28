92c81766a8f33cbfb1b015e5ba490a51a18960e4
Santiago L. Valdarrama <svpino@gmail.com>
2021-11-28 14:47:57 -0500

    Added traits and finalized the card design

--- card/script/generate.py
+++ card/script/generate.py
@@ -6,31 +6,6 @@ import os
 import numpy as np
 
 
-# Source: https://venngage.com/blog/pastel-color-palettes/#1
-# COLOR_PALETTE_POOL = [
-#     ["edf2fb", "d7e3fc", "ccdbfd", "c1d3fe", "abc4ff"],
-#     ["d1d1d1", "e1dbd6", "e2e2e2", "f9f6f2", "ffffff"],
-#     ["ffc09f", "ffee93", "fcf5c7", "a0ced9", "adf7b6"],
-#     ["809bce", "95b8d1", "b8e0d2", "d6eadf", "eac4d5"],
-#     ["e8d1c5", "eddcd2", "fff1e6", "f0efeb", "eeddd3"],
-#     ["e8dff5", "fce1e4", "fcf4dd", "ddedea", "daeaf6"],
-#     ["d4afb9", "d1cfe2", "9cadce", "7ec4cf", "52b2cf"],
-#     ["d3ab9e", "eac9c1", "ebd8d0", "fffbff", "fefeff"],
-#     ["ffe5ec", "ffc2d1", "ffb3c6", "ff8fab", "fb6f92"],
-#     ["79addc", "ffc09f", "ffee93", "fcf5c7", "adf7b6"],
-#     ["ffffff", "84dcc6", "a5ffd6", "ffa69e", "ff686b"],
-#     ["61f4de", "65cbe9", "68b6ef", "6c8dfa", "6e78ff"],
-#     ["ff7477", "e69597", "ceb5b7", "b5d6d6", "9cf6f6"],
-#     ["f49097", "dfb2f4", "f5e960", "f2f5ff", "55d6c2"],
-#     ["e27396", "ea9ab2", "efcfe3", "eaf2d7", "b3dee2"],
-#     ["f55c7a", "f57c73", "f68c70", "f6ac69", "f6bc66"],
-#     ["f08080", "f4978e", "f8ad9d", "fbc4ab", "ffdab9"],
-#     ["cdb4db", "ffc8dd", "ffafcc", "bde0fe", "a2d2ff"],
-#     ["fdffb6", "caffbf", "9bf6ff", "a0c4ff", "ffc6ff"],
-#     ["a7bed3", "c6e2e9", "f1ffc4", "ffcaaf", "dab894"],
-#     ["70d6ff", "ff70a6", "ff9770", "ffd670", "e9ff70"],
-# ]
-
 COLOR_PALETTE_POOL = [
     ["69d2e7", "a7dbd8", "e0e4cc", "f38630", "fa6900"],
     ["fe4365", "fc9d9a", "f9cdad", "c8c8a9", "83af9b"],
@@ -1034,7 +1009,7 @@ def screenshot(driver, number, rotatey, rotatez, rotategrad, color1, color2, col
     driver.get(page_url)
 
     element = driver.find_element_by_id("main-element")
-    element.screenshot(f"card/images/{number:03d}.png")
+    element.screenshot(f"card/images/{number:03d}-{doodle}")
 
 def main():
     chrome_options = Options()
@@ -1043,12 +1018,10 @@ def main():
         "card/script/driver/chromedriver", chrome_options=chrome_options
     )
 
-    doodles = os.listdir("card/images/pfp/")
+    doodles = os.listdir("card/pfp/")
     np.random.shuffle(doodles)
 
-    print(doodles)
-
-    for i in range(0, 10):
+    for i in range(0, 1000):
         random.seed(i)
 
         rotatey = random.randint(-20, 20)
@@ -1060,7 +1033,7 @@ def main():
         random.shuffle(palette)
 
         screenshot(
-            driver, i, rotatey, rotatez, rotategrad, palette[0], palette[1], palette[2], doodle
+            driver, i + 1, rotatey, rotatez, rotategrad, palette[0], palette[1], palette[2], doodle
         )
 
     driver.close()
--- card/script/pfp.py
+++ card/script/pfp.py
@@ -1,15 +1,16 @@
 import os
+import json
 import numpy as np
 from PIL import Image, ImageDraw, ImageFont
 
 TOTAL = 2000
 
 TRAITS = {
-    "skin": [0.2, 0.2, 0.2, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05],
-    "shirt": [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1],
-    "shirt-accessories": [0.5, 0.1, 0.1, 0.1, 0.1, 0.05, 0.05],
-    "headband": [0.8, 0.07, 0.06, 0.04, 0.03],
-    "expression": [
+    "Skin": [0.2, 0.2, 0.2, 0.10, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05],
+    "Shirt": [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1],
+    "Shirt Accessories": [0.5, 0.1, 0.1, 0.1, 0.1, 0.05, 0.05],
+    "Headband": [0.8, 0.07, 0.06, 0.04, 0.03],
+    "Expression": [
         0.15,
         0.15,
         0.15,
@@ -24,40 +25,64 @@ TRAITS = {
         0.02,
         0.01,
     ],
-    "hair": [0.4, 0.2, 0.15, 0.15, 0.10],
+    "Hair": [0.4, 0.2, 0.15, 0.15, 0.10],
     "lineart": [1.0],
-    "accessories": [0.4, 0.1, 0.1, 0.1, 0.1, 0.05, 0.05, 0.05, 0.05],
+    "Accessories": [0.4, 0.1, 0.1, 0.1, 0.1, 0.05, 0.05, 0.05, 0.05],
 }
 
+def load_png_files_from_directory(directory):
+    files = []
+    for filename in sorted(os.listdir(directory)):
+        if filename.endswith(".png"):
+            files.append(filename)
 
-def load_trait(name, trait):
-    index = np.random.choice(np.arange(0, len(trait)), p=trait)
-    image = Image.open(f"card/pfp/{name}/{index:02d}.png")
-    return image, index
+    return files
+
+
+def load_trait(name):
+    files = load_png_files_from_directory(f"card/traits/{name}")
+    filename = np.random.choice(files, p=TRAITS[name])
+    image = Image.open(f"card/traits/{name}/{filename}")
+    return image, filename
 
 
 def create_doodle(index):
     properties = []
 
-    background, index = load_trait("skin", TRAITS["skin"])
-    properties.append(index)
+    background, filename = load_trait("Skin")
+    properties.append(filename)
 
     for name, trait in TRAITS.items():
-        if name == "skin":
+        if name == "Skin":
             continue
 
-        foreground, index = load_trait(name, trait)
+        foreground, filename = load_trait(name)
         background.paste(foreground, (0, 0), foreground)
 
-        properties.append(index)
+        properties.append(filename)
 
-    filename = "".join(map(lambda x: f"{x:02d}", properties))
-    background.save(f"card/images/pfp/{filename}.png")
+    doodle = "".join(map(lambda x: f"{x[0:2]}", properties))
+    background.save(f"card/pfp/{doodle}.png")
+
+    return doodle, properties
 
 
 def main():
-    for i in range(TOTAL):
-        create_doodle(i)
+    metadata = {}
+
+    for doodle_number in range(TOTAL):
+        doodle, properties = create_doodle(doodle_number)
+
+        metadata[doodle] = []
+        for index, trait in enumerate(TRAITS.keys()):
+            if trait == "lineart":
+                continue
+
+            value = properties[index][5:-4]
+            metadata[doodle].append({"trait_type": trait, "value": value})
+        
+    with open("card/pfp/metadata.json", "w") as f:
+        f.write(json.dumps(metadata))
 
 
 if __name__ == "__main__":
